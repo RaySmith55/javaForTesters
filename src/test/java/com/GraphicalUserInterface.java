8e9cb982fa0e85bbf660206d377de6b9da17cb1f
Ray Smith <ray.smith@sky.uk>
2019-01-04 14:12:24 +0000

    Add GraphicalUserInterface class

---
+++ src/test/java/com/GraphicalUserInterface.java
@@ -0,0 +1,47 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphicalUserInterface {
+
+    public static void main(String args[]) {
+
+        // Creating the frame
+        JFrame frame = new JFrame("Chat Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+
+        // Creating the MenuBar and adding components
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("FILE");
+        JMenu m2 = new JMenu("Help");
+        mb.add(m1);
+        mb.add(m2);
+        JMenuItem m11 = new JMenuItem("Open");
+        JMenuItem m22 = new JMenuItem("Save as");
+        m1.add(m11);
+        m1.add(m22);
+
+        // Creating the panel at bottom and adding components
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Enter text");
+        JTextField tf = new JTextField(10);
+        JButton send = new JButton("Send");
+        JButton reset = new JButton("Reset");
+        panel.add(label);
+        panel.add(label);
+        panel.add(tf);
+        panel.add(send);
+        panel.add(reset);
+
+        // Text area at the centre
+        JTextArea ta = new JTextArea();
+
+        // Adding components to the frame
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, ta);
+        frame.setVisible(true);
+    }
+}
