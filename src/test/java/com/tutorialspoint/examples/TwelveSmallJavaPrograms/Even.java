aedeacca8d19f4306c33ece2a1baf378d59f18e1
Ray Smith <ray.smith@sky.uk>
2019-02-21 14:49:21 +0000

    Start of 12 small java programs

---
+++ src/test/java/com/tutorialspoint/examples/TwelveSmallJavaPrograms/Even.java
@@ -0,0 +1,10 @@
+package com.tutorialspoint.examples.TwelveSmallJavaPrograms;
+
+public class Even {
+
+    public static void main(String[] args) {
+        for(int num = 0; num <= 100; num = num + 2) {
+            System.out.println(num);
+        }
+    }
+}
---
+++ src/test/java/com/tutorialspoint/examples/TwelveSmallJavaPrograms/EvenOrOdd.java
@@ -0,0 +1,17 @@
+package com.tutorialspoint.examples.TwelveSmallJavaPrograms;
+
+public class EvenOrOdd {
+
+    public static void main(String[] args) {
+        int number, rem;
+
+        number = 7;
+        rem = number % 2;
+
+        if(rem == 0) {
+            System.out.println("Number is even: " + number);
+        } else {
+                System.out.println("Number is odd: " + number);
+        }
+    }
+}
